2c97fd5e3551dd5cb95c7396dc3d6db5dae1673e
timeofforest <dojihwan1030@gmail.com>
2021-09-15 13:49:49 +0900

    first commit

---
+++ homework1copy.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+int main(void) {
+    printf("Somebody once said, \"Who\'s responsible for the riots?");
+    printf("\nThe rioters.\"");
+    printf("\n\'A\' and \"a\" mean different things.");
+    printf("\nA backslash, \\, preceding a characer is called");
+    printf("\nYou must escape the ability of the \" to terminate");
+    printf("\na string constant");
+    printf("\nby using \\\", or the ability of the \\ to escape, by using \\\\.");
+    printf("\nA strary \\, say \\z, in a string constant will have");
+    printf("\ndifferent effects on different compilers.");
+    printf("\n\\n is a new line character.");
+    printf("\n\\r a carriage return");
+    printf("\n\\t a tab");
+    printf("\n\\a an alert");
+    printf("\n\\\\ a backslash");
+    printf("\n\\\' a single quote");
+    printf("\n\\\" a double quote");
+    printf("\n\"\\n\\r\\t\\a\\\\\\\'\\\"\" is a string of escape sequence characters.");
+    
+    return 0; 
+}
